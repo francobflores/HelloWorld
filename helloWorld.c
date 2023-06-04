4947d1aca3e324561ee3c9d9c7f97b442d4b9464
Franco Flores <francobarbosaflores@gmail.com>
2023-06-04 20:32:01 -0300

    complete

---
+++ helloWorld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/* function print Hello World! */
+void helloWorld(){
+    printf("Hello World Friends! \n");
+}
+/* function main */
+int main(){
+    helloWorld();
+    return 0;
+}
